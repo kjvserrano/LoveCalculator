3adfbcab856dca318160908fc39f1313977ccc4a
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-24 00:48:57 +0800

    Initial commit of flames calculator

---
+++ LoveCalculator/src/main/loveServer/FlamesCalculator.java
@@ -0,0 +1,11 @@
+package main.loveServer;
+
+public class FlamesCalculator extends LoveCalculator {
+
+	@Override
+	public String calculate(String inputA, String inputB) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ LoveCalculator/src/main/loveServer/LoveCalculator.java
@@ -0,0 +1,5 @@
+package main.loveServer;
+
+public abstract class LoveCalculator {
+	public abstract String calculate(String inputA, String inputB);
+}
