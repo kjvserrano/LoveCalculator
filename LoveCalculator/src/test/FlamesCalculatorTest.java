f122327a5d43b31bc7c5cb20dcb7000c2f6351fe
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-24 01:25:52 +0800

    Tests for FlamesCount spaces and same name

--- LoveCalculator/src/test/FlamesCalculatorTest.java
+++ LoveCalculator/src/test/FlamesCalculatorTest.java
@@ -9,6 +9,7 @@ import main.loveServer.FlamesCalculator;
 public class FlamesCalculatorTest {
 	FlamesCalculator flamesUUT = new FlamesCalculator();
 	
+	//FlamesCountTest
 	@Test
 	public void flamesCountLowerCase(){
 		int flamesCount = flamesUUT.flamesCount("bob", "joe");
@@ -18,6 +19,16 @@ public class FlamesCalculatorTest {
 	public void flamesCountUpperLowerCase(){
 		int flamesCount = flamesUUT.flamesCount("Bobby", "Jobs");
 		assertEquals(3,flamesCount);
+	}
+	@Test
+	public void flamesCountSpace(){
+		int flamesCount = flamesUUT.flamesCount("Bobby Jones", "Steve Jobs");//"yn", "tv"
+		assertEquals(4,flamesCount);
+	}
+	@Test
+	public void flamesCountZero(){
+		int flamesCount = flamesUUT.flamesCount("Bobby Jones", "Bobby Jones");//"yn", "tv"
+		assertEquals(0,flamesCount);
 	}
 
 }
