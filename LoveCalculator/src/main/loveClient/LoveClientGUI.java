a20d4d41bdd766e5e93c9c9ab277e655f9dd699d
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-24 00:09:37 +0800

    Implement request and display functions for client GUI

--- LoveCalculator/src/main/loveClient/LoveClientGUI.java
+++ LoveCalculator/src/main/loveClient/LoveClientGUI.java
@@ -1,5 +1,16 @@
 package main.loveClient;
 
-public class LoveClientGUI {
+import java.util.Scanner;
 
+public class LoveClientGUI {
+	Scanner scanner = new Scanner(System.in);
+	
+	public String requestForInput(){
+		System.out.println("Enter two names to see their love compatibility (eg Bob,Joe): ");
+		return scanner.nextLine();
+	}
+	
+	public void printResult(String result){
+		System.out.println("Result: " + result);
+	}
 }
