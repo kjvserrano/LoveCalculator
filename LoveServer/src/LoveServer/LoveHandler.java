e8c482e8f6a7244d4cd9d12b967f137ebb8220c5
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-23 19:04:20 +0800

    Commit multithreaded server

---
+++ LoveServer/src/LoveServer/LoveHandler.java
@@ -0,0 +1,54 @@
+package LoveServer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class LoveHandler extends Thread {
+	private Socket clientSocket = null;
+	private DataInputStream clientInputStream;
+	private DataOutputStream clientOutputStream;
+	
+	public LoveHandler(Socket clientSocket) throws IOException{
+		this.clientSocket = clientSocket;
+		clientInputStream = new DataInputStream(this.clientSocket.getInputStream());
+		clientOutputStream = new DataOutputStream(this.clientSocket.getOutputStream());
+	}
+	
+	public void run(){
+		String message = "";
+		try {
+			message = readFromClient();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		handleRequest(message);
+
+	}
+	
+	public void handleRequest(String message){
+		//ADD Calculator Code Here
+		try {
+			sendToClient(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String readFromClient() throws IOException{
+		String message = clientInputStream.readUTF();
+		System.out.println(message);
+		return message;
+	}
+	
+	public void sendToClient(String message) throws IOException{
+		clientOutputStream.writeBytes(message);
+	}
+	
+	public void closeConnection() throws IOException{
+		clientSocket.close();
+	}
+}
---
+++ LoveServer/src/LoveServer/LoveServer.java
@@ -0,0 +1,28 @@
+package LoveServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class LoveServer {
+	private ServerSocket listenSocket;
+	
+	public LoveServer() throws IOException{
+		listenSocket = new ServerSocket(5000);
+		listenSocket.setSoTimeout(1000);
+	}
+	
+	public LoveServer(ServerSocket serverSocket) throws IOException{
+		this.listenSocket = serverSocket;
+		this.listenSocket.setSoTimeout(1000);
+	}
+	
+	public void startServer() throws IOException{
+		while(true){
+			Socket clientSocket = listenSocket.accept();
+			LoveHandler loveHandler = new LoveHandler(clientSocket);
+			loveHandler.start();
+		}
+	}
+
+}
