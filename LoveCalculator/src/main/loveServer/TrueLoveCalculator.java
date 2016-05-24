6f7f86761d9fc06d374b806e814dbdeab0f6bb44
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-24 19:03:06 +0800

    Initial commit for TrueLove Calculator

---
+++ LoveCalculator/src/main/loveServer/TrueLoveCalculator.java
@@ -0,0 +1,15 @@
+package main.loveServer;
+
+public class TrueLoveCalculator extends LoveCalculator {
+
+	public TrueLoveCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String calculate(String inputA, String inputB) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
