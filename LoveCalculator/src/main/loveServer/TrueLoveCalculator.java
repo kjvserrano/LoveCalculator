571941f4d9aefaedede5d6ee2246713297122363
Josen Daniel De Leon <jddl68@gmail.com>
2016-05-24 23:48:03 +0800

    Implement truelovecalculate

--- LoveCalculator/src/main/loveServer/TrueLoveCalculator.java
+++ LoveCalculator/src/main/loveServer/TrueLoveCalculator.java
@@ -12,4 +12,23 @@ public class TrueLoveCalculator extends LoveCalculator {
 		return null;
 	}
 
+	public int trueLoveCounter(String paramA, String paramB, String compStr){
+		String inputA = paramA.toLowerCase();
+		String inputB = paramB.toLowerCase();
+		int sum = 0;
+		
+		for(char retval:compStr.toCharArray()){
+			for(int i = 0; i<inputA.length(); i++){
+				if(inputA.charAt(i)==retval){
+					sum++;
+				}
+			}
+			for(int i = 0; i<inputB.length(); i++){
+				if(inputA.charAt(i)==retval){
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
 }
--- LoveCalculator/src/test/TrueLoveCalculatorTest.java
+++ LoveCalculator/src/test/TrueLoveCalculatorTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TrueLoveCalculatorTest {
+import main.loveServer.TrueLoveCalculator;
 
+public class TrueLoveCalculatorTest {
+	TrueLoveCalculator trueLoveUUT = new TrueLoveCalculator();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void trueCalculateTomShelly(){
+		int res = trueLoveUUT.trueLoveCounter("Tom Thompson", "Shelly Smith", "true");
+		assertEquals(4,res);
+	}
+	
+	@Test
+	public void loveCalculateTomShelly(){
+		int res = trueLoveUUT.trueLoveCounter("Tom Thompson", "Shelly Smith", "love");
+		assertEquals(6,res);
 	}
 
 }
